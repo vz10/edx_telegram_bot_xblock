b5e8f55054f02952feccfb1d358e828d94f58d91
Ilya Batozskiy <vZ@vZs-iMac-3.local>
2016-05-21 22:55:23 +0300

    Remove extra buttons from "Add component" menu

--- bot_xblock/static/js/src/studio.js
+++ bot_xblock/static/js/src/studio.js
@@ -1,87 +1,4 @@
-/**
- * Created by vZ on 5/17/16.
- */
-function BotXBlockStudio(runtime, element) {
 
-    function consoleResult(result) {
-       console.log(result);
-    }
-
-    var handlerUrl = runtime.handlerUrl(element, 'xblock_move');
-
-    $(".reorderable-container").on("sortupdate", function(eventObject, ui) {
-        var theoretical_part = $('#theoretical_part').children('li.studio-xblock-wrapper ').length; 
-        var question_part = $('#question_part').children('li.studio-xblock-wrapper ').length;
-        var positive_part = $('#positive_part').children('li.studio-xblock-wrapper ').length;
-        var negative_part = $('#negative_part').children('li.studio-xblock-wrapper ').length;
-        $(".component-placeholder").remove();
-        $.ajax({
-            type: "POST",
-            url: handlerUrl.replace("preview/",''),
-            data: JSON.stringify({"theoretical_part": theoretical_part, 
-                                  "question_part": question_part,
-                                  "positive_part": positive_part,
-                                  "negative_part": negative_part}),
-            async: false,
-            success: consoleResult
-        });
-    });
-
-    $( ".reorderable-container" ).on( "sortstart", function( event, ui ) {
-        $("ol:not(:has(li))").append('<li class="component-placeholder"><h3>Put something here</h3></li>')
-    } );
-
-   // $(".drag-handle").on("mouseover", function(){console.log('asdfsdf')});
-    $(function ($) {
-        /* Here's where you'd do things on page load. */
-        console.log('')
-        $('button[data-type="discussion"]').hide();
-        $('button[data-type="advanced"]').hide();
-    });
-}
-
-
-/**
- * Created by vZ on 5/17/16.
- */
-function BotXBlockStudio(runtime, element) {
-
-    function consoleResult(result) {
-       console.log(result);
-    }
-
-    var handlerUrl = runtime.handlerUrl(element, 'xblock_move');
-
-    $(".reorderable-container").on("sortupdate", function(eventObject, ui) {
-        var theoretical_part = $('#theoretical_part').children('li.studio-xblock-wrapper ').length; 
-        var question_part = $('#question_part').children('li.studio-xblock-wrapper ').length;
-        var positive_part = $('#positive_part').children('li.studio-xblock-wrapper ').length;
-        var negative_part = $('#negative_part').children('li.studio-xblock-wrapper ').length;
-        $(".component-placeholder").remove();
-        $.ajax({
-            type: "POST",
-            url: handlerUrl.replace("preview/",''),
-            data: JSON.stringify({"theoretical_part": theoretical_part, 
-                                  "question_part": question_part,
-                                  "positive_part": positive_part,
-                                  "negative_part": negative_part}),
-            async: false,
-            success: consoleResult
-        });
-    });
-
-    $( ".reorderable-container" ).on( "sortstart", function( event, ui ) {
-        $("ol:not(:has(li))").append('<li class="component-placeholder"><h3>Put something here</h3></li>')
-    } );
-
-   // $(".drag-handle").on("mouseover", function(){console.log('asdfsdf')});
-    $(function ($) {
-        /* Here's where you'd do things on page load. */
-        console.log('')
-        $('button[data-type="discussion"]').hide();
-        $('button[data-type="advanced"]').hide();
-    });
-}
 
 function check_xblocks_type() {
     $('section').removeClass('wrong_xblock');
@@ -127,6 +44,10 @@ function BotXBlockStudio(runtime, element) {
         $("ol:not(:has(li))").append('<li class="component-placeholder"><h3>Put something here</h3></li>')
     } );
 
+        $('#negative_part').on('DOMNodeInserted', '.studio-xblock-wrapper', function () {
+        check_xblocks_type();
+    });
+
    // $(".drag-handle").on("mouseover", function(){console.log('asdfsdf')});
     $(function ($) {
         /* Here's where you'd do things on page load. */
@@ -134,5 +55,3 @@ function BotXBlockStudio(runtime, element) {
 
     });
 }
-
-
